c7edf23288101cafa2cd0cbe3704cdaaa5dd1562
Kaelan Fouwels <kfouwels@gmail.com>
2012-12-08 14:08:28 +0000

    final

---
+++ Loaders.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace kfouwels.lib.SentimentAnalysis
+{
+    public class loader
+    {
+        public Dictionary<string,sbyte> loadDictionaryFromTxt(string pathToWordListFile)
+        {
+            StreamReader sr = new StreamReader(pathToWordListFile);
+
+            char[] SeparatorChars = new char[] {' ', '\r', '\n'};     //BROKEN? - CHECK THIS FIRST!                
+            string justReadInLine = null;
+            int lengthOfFile = Convert.ToInt32(sr.ReadLine());
+
+            Dictionary<string, sbyte> loadedWordList = new Dictionary<string, sbyte>();
+
+            while (sr.Peek() != '~')
+            {
+                justReadInLine = sr.ReadLine().ToLower();
+                loadedWordList.Add(justReadInLine.Split(SeparatorChars)[0].ToString(), Convert.ToSByte(justReadInLine.Split(SeparatorChars)[1].ToString()));
+            }
+
+            sr.Close();
+            return loadedWordList;        
+        }
+        public Dictionary<string, sbyte> loadDictionaryFromTxt(string pathToWordListFile, char[] separatorChars)
+        {
+            StreamReader sr = new StreamReader(pathToWordListFile);
+       
+            string justReadInLine = null;
+            int lengthOfFile = Convert.ToInt32(sr.ReadLine());
+
+            Dictionary<string, sbyte> loadedWordList = new Dictionary<string, sbyte>();
+
+            while (sr.Peek() != '~')
+            {
+                justReadInLine = sr.ReadLine().ToLower();
+                loadedWordList.Add(justReadInLine.Split(separatorChars)[0].ToString(), Convert.ToSByte(justReadInLine.Split(separatorChars)[1]));
+            }
+
+            sr.Close();
+            return loadedWordList;
+        }
+        public String loadStringFromTxt(string pathToWordListFile)
+        {
+            StreamReader sr2 = new StreamReader(pathToWordListFile);
+            string tempstring2 = sr2.ReadToEnd().ToLower();
+            sr2.Close();
+
+            return tempstring2;            
+        }
+        public String[] loadStringArrayFromTxt(string pathToWordListFile)
+        {
+            StreamReader sr2 = new StreamReader(pathToWordListFile);
+            char[] separators = new char[] {' ', '\r', '\n'};
+            string tempString = sr2.ReadToEnd().ToLower();
+
+            return tempString.Split(separators, System.StringSplitOptions.RemoveEmptyEntries);
+        }
+        public String[] loadStringArrayFromTxt(string pathToWordListFile, char[] separatorChars)
+        {
+            StreamReader sr2 = new StreamReader(pathToWordListFile);
+            string tempString = sr2.ReadToEnd().ToLower();
+
+            return tempString.Split(separatorChars);
+        }
+    }
+}
---
+++ SentimentAnalysis.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace kfouwels.lib.SentimentAnalysis
+{
+    public class SentimentAnalyser
+    {
+        Dictionary<string, sbyte> wordlist;
+        Dictionary<string, sbyte> inverters;
+        Dictionary<string, sbyte> intensifiers;
+
+        public SentimentAnalyser(Dictionary<string, sbyte> wordlistIn, Dictionary<string, sbyte> invertersIn, Dictionary<string, sbyte> intensifiersIn)
+        {
+            wordlist = wordlistIn; 
+            inverters = invertersIn;
+            intensifiers = intensifiersIn;
+        }
+        public decimal Analyse(string[] word)
+        {
+            decimal SentimentValue = 0; //Master value of awesomeness
+            decimal wordsFound = 0;
+            decimal v = 0;              //Temp value for calculations
+            decimal w = 0;              //Temp value for calculations
+            
+            for (int wordCycler = 0; wordCycler < word.Length; wordCycler++) //Cycle word
+            {
+                Console.WriteLine("processing {0}", word[wordCycler]);
+                if (wordlist.ContainsKey(word[wordCycler]) == true)
+                {
+                    wordsFound++;
+                    if (inverters.ContainsKey(word[wordCycler - 1]) == true)
+                    {
+                        if (intensifiers.ContainsKey(word[wordCycler - 2]) == true)
+                        {
+                            //intensifiers - inverters - word
+                            w = wordlist[word[wordCycler]];
+                            v = intensifiers[word[wordCycler]];
+
+                            SentimentValue += ((w + (w * v / 100)) * -1);
+                        }
+                        else
+                        {
+                            //inverters - word
+                            SentimentValue += (wordlist[word[wordCycler]] * -1); // -1 inverts
+                        }
+                    
+                    }
+                    else
+                    {
+                        if (intensifiers.ContainsKey(word[wordCycler - 1]) == true)
+                        {
+                            if (inverters.ContainsKey(word[wordCycler - 2]) == true)
+                            {
+                                //inverters - intensifiers - word
+
+                                w = wordlist[word[wordCycler]];
+                                v = intensifiers[word[wordCycler]];
+
+                                SentimentValue += ((w + (w * v / 100)) * -1);
+                            }
+                            else
+                            {
+                                // intensifiers - word
+                                //+= (w + (w * v /100))
+
+                                w = wordlist[word[wordCycler]];
+                                v = intensifiers[word[wordCycler]];
+
+                                SentimentValue += (w + (w * v / 100));                                
+                            }
+                        }
+                        else
+                        {
+                            //word
+                            SentimentValue += wordlist[word[wordCycler]];
+                        }
+                    }
+                }
+            } // end For
+
+            return (SentimentValue / wordsFound);
+        }
+    }
+}
