7446648daeb8d8e88e00889902d8ada367124b91
Kaelan Fouwels <kfouwels@gmail.com>
2013-01-25 08:05:33 +0000

    Code cleanup

--- SentimentAnalysisWin32Library/SentimentAnalysis.cs
+++ SentimentAnalysisWin32Library/SentimentAnalysis.cs
@@ -1,87 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
 
 namespace kfouwels.lib.SentimentAnalysis
 {
     public class SentimentAnalyser
     {
-        Dictionary<string, sbyte> wordlist;
-        Dictionary<string, sbyte> inverters;
-        Dictionary<string, sbyte> intensifiers;
+        private readonly Dictionary<string, sbyte> _intensifiers;
+        private readonly Dictionary<string, sbyte> _inverters;
+        private readonly Dictionary<string, sbyte> _wordlist;
 
-        public SentimentAnalyser(Dictionary<string, sbyte> wordlistIn, Dictionary<string, sbyte> invertersIn, Dictionary<string, sbyte> intensifiersIn)
+        public SentimentAnalyser(Dictionary<string, sbyte> wordlistIn, Dictionary<string, sbyte> invertersIn,
+                                 Dictionary<string, sbyte> intensifiersIn)
         {
-            wordlist = wordlistIn; 
-            inverters = invertersIn;
-            intensifiers = intensifiersIn;
+            _wordlist = wordlistIn;
+            _inverters = invertersIn;
+            _intensifiers = intensifiersIn;
         }
+
         public decimal Analyse(string[] word)
         {
-            decimal SentimentValue = 0; //Master value of awesomeness
+            decimal sentimentValue = 0; //Master value of awesomeness
             decimal wordsFound = 0;
-            decimal v = 0;              //Temp value for calculations
-            decimal w = 0;              //Temp value for calculations
-            
+            decimal v; //Temp value for calculations
+            decimal w; //Temp value for calculations
+
             for (int wordCycler = 0; wordCycler < word.Length; wordCycler++) //Cycle word
             {
                 Console.WriteLine("processing {0}", word[wordCycler]);
-                if (wordlist.ContainsKey(word[wordCycler]) == true)
+                if (_wordlist.ContainsKey(word[wordCycler]))
                 {
                     wordsFound++;
-                    if (inverters.ContainsKey(word[wordCycler - 1]) == true)
+                    if (_inverters.ContainsKey(word[wordCycler - 1]))
                     {
-                        if (intensifiers.ContainsKey(word[wordCycler - 2]) == true)
+                        if (_intensifiers.ContainsKey(word[wordCycler - 2]))
                         {
                             //intensifiers - inverters - word
-                            w = wordlist[word[wordCycler]];
-                            v = intensifiers[word[wordCycler]];
+                            w = _wordlist[word[wordCycler]];
+                            v = _intensifiers[word[wordCycler]];
 
-                            SentimentValue += ((w + (w * v / 100)) * -1);
+                            sentimentValue += ((w + (w*v/100))*-1);
                         }
                         else
                         {
                             //inverters - word
-                            SentimentValue += (wordlist[word[wordCycler]] * -1); // -1 inverts
+                            sentimentValue += (_wordlist[word[wordCycler]]*-1); // -1 inverts
                         }
-                    
                     }
                     else
                     {
-                        if (intensifiers.ContainsKey(word[wordCycler - 1]) == true)
+                        if (_intensifiers.ContainsKey(word[wordCycler - 1]))
                         {
-                            if (inverters.ContainsKey(word[wordCycler - 2]) == true)
+                            if (_inverters.ContainsKey(word[wordCycler - 2]))
                             {
                                 //inverters - intensifiers - word
 
-                                w = wordlist[word[wordCycler]];
-                                v = intensifiers[word[wordCycler]];
+                                w = _wordlist[word[wordCycler]];
+                                v = _intensifiers[word[wordCycler]];
 
-                                SentimentValue += ((w + (w * v / 100)) * -1);
+                                sentimentValue += ((w + (w*v/100))*-1);
                             }
                             else
                             {
                                 // intensifiers - word
                                 //+= (w + (w * v /100))
 
-                                w = wordlist[word[wordCycler]];
-                                v = intensifiers[word[wordCycler]];
+                                w = _wordlist[word[wordCycler]];
+                                v = _intensifiers[word[wordCycler]];
 
-                                SentimentValue += (w + (w * v / 100));                                
+                                sentimentValue += (w + (w*v/100));
                             }
                         }
                         else
                         {
                             //word
-                            SentimentValue += wordlist[word[wordCycler]];
+                            sentimentValue += _wordlist[word[wordCycler]];
                         }
                     }
                 }
             } // end For
 
-            return (SentimentValue / wordsFound);
+            return (sentimentValue/wordsFound);
         }
     }
 }
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using kfouwels;
 using kfouwels.lib.SentimentAnalysis;
 
 //Load wordlist >> Dictionary[]
